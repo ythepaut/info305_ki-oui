40494434c1b36915ed3942491aa6c2ab63d20b8e
LucCha42 <55276692+LucCha42@users.noreply.github.com>
2019-11-08 13:05:55 +0100

    Add files via upload

--- includes/pages/accueil.php
+++ includes/pages/accueil.php
@@ -1,49 +1,51 @@
-	<section class="pair">
+<section class="pair container-fluid">
+	<div class="row ligne0">
 
-        <div >
+		<div class="inner col-lg-5">
+			<h1>Partagez vos fichiers en toute tranquilité</h1>
+			<p>Chez Ki-Oui, l'hébergement de vos fichiers est crypté.<br/>
+			Partagez-les avec un lien auto-généré comme bon vous semble.<br/>
+			Créez-vous un compte pour répertorier vos fichiers.</p>
+			<a href="" class="button"><i class="fas fa-pen"></i> &nbsp; S'inscrire</a>
+		</div>
 
-            <h3>KI-OUI, dites oui à qui ? A KI-OUI ! Ah ah, à nous quoi. Parce que ki ça fait qui et comme on s'appelle KI-OUI tout ça...</h3> <sub>(tuez-moi)</sub>
-            <br />
-            <br />
-            <br />
-            <p>
+		<div class="inner col-lg-1"></div>
 
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Je ne cautionne pas ce slogan. Quia quo quos nulla labore voluptatum animi sunt minima? Placeat deleniti rerum veritatis architecto quod. Aut, assumenda eligendi nisi molestias repellendus minus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum sit ipsa ab qui nobis libero, inventore quae aspernatur quibusdam, provident iusto quod amet porro. Ipsam cumque repudiandae repellendus labore earum!
-            </p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Et sunt iste at illum placeat. Je suis si seul :c ... Exercitationem, ea quisquam? Recusandae, odio saepe dignissimos maxime doloribus accusantium, quas ipsam vel cum vitae rerum.</p>
-            <button>S'inscrire !</button>
-        </div>
-
-    </section>
-
-	<section class="impair">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum quisquam nobis accusamus exercitationem itaque, error velit illo aliquam nisi temporibus consequatur eveniet illum, tempore qui aspernatur, voluptate magnam. Numquam, sed. Ahah regarde Lucas je passe à la télé sur la page d'accueil &#127875;
-    </section>
-
-    <section class="pair">
-
-        <div >
-
-            <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia quo quos nulla labore voluptatum animi sunt minima? Placeat deleniti rerum veritatis architecto quod. Aut, assumenda eligendi nisi molestias repellendus minus.
-            </p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Et sunt iste at illum placeat. Exercitationem, ea quisquam? Recusandae, odio saepe dignissimos maxime doloribus accusantium, quas ipsam vel cum vitae rerum.</p>
-            <button>S'inscrire !</button>
-        </div>
-
-    </section>
-
-	<section class="impair">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum quisquam nobis accusamus exercitationem itaque, error velit illo aliquam nisi temporibus consequatur eveniet illum, tempore qui aspernatur, voluptate magnam. Numquam, sed.
-    </section>
-
-	<section class="pair">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum pariatur deleniti porro amet culpa aspernatur libero laudantium, praesentium atque reprehenderit provident recusandae officia sapiente unde similique id corrupti earum qui?
-
-        <br />
-
-        <a href="/ajout" class="button">Ajouter un fichier</a>
-
-    </section>
+	</div>
+</section>
 
 
+
+<section class="impair container-fluid">
+	<div class="row ligne1">
+
+		<div class="inner col-lg-4">
+			<h2>Partage Sécurisé</h2>
+			<i class="fas fa-unlock-alt"></i>
+		</div>
+
+		<div class="inner col-lg-4">
+			<h2>Authentification Simple</h2>
+			<i class="fas fa-user"></i>
+		</div>
+
+		<div class="inner col-lg-4">
+			<h2>Faire un don</h2>
+			<i class="fas fa-euro-sign"></i>
+		</div>
+
+	</div>
+</section>
+
+
+
+<section class="pair">
+
+	<div class="inner">
+		<a href="/ajout" class="button"><i class="fas fa-file-import"></i> &nbsp; Ajouter un fichier</a>
+	</div>
+
+</section>
+
+
+<?php include("./includes/pages/modals/login.php"); ?>
