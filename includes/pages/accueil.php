00f2662cec647fbf9fb6113b3e5c8687fa8c74c8
LucCha42 <55276692+LucCha42@users.noreply.github.com>
2019-11-08 16:14:30 +0100

    Update accueil.php

--- includes/pages/accueil.php
+++ includes/pages/accueil.php
@@ -18,9 +18,15 @@
 	<div class="row ligne1">
 
 		<div class="inner col-lg-4">
-			<h2>Partage Sécurisé</h2>
+			<h2>Hébergement Sécurisé</h2>
 			<i class="fas fa-unlock-alt"></i>
-			<p>Vos données sont cryptées,<br/>Ki-Oui ne pourra pas accéder à vos fichiers.</p>
+			<p>Chez Ki-Oui, les données de vos fichiers sont cryptées.<br/>Nous ne pourront pas accéder à votre contenue.</p>
+		</div>
+
+		<div class="inner col-lg-4">
+			<h2>Partage Par Lien</h2>
+			<i class="fas fa-link"></i>
+			<p>Partagez vos fichiers avec un lien auto-généré.<br/>Chaque lien est unique à chaque fichier.</p>
 		</div>
 
 		<div class="inner col-lg-4">
@@ -29,27 +35,16 @@
 			<p>Une adresse mail,<br/>un mote de passe.<br/>Rien de plus.</p>
 		</div>
 
-		<div class="inner col-lg-4">
-			<h2>Faire un don</h2>
-			<i class="fas fa-euro-sign"></i>
-			<p>Parce que nous sommes étudiants <3</p>
-		</div>
 
 	</div>
 </section>
 
 
-<<<<<<< HEAD
-=======
 
 <section class="pair container-fluid">
 	<div class="row ligne2">
 
 		<div class="inner col">
-			<p>Chez Ki-Oui, l'hébergement de vos fichiers est crypté.<br/>
-			Partagez-les avec un lien auto-généré comme bon vous semble.<br/>
-			Créez-vous un compte pour répertorier vos fichiers.</p>
-			<br/>
 			<a href="/ajout" class="button"><i class="fas fa-file-import"></i> &nbsp; Ajouter un fichier</a>
 		</div>
 
@@ -57,5 +52,4 @@
 </section>
 
 
->>>>>>> 680459a64e46fcb8f86b42ae8515d45f297ab0a9
 <?php include("./includes/pages/modals/login.php"); ?>
