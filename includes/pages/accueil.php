680459a64e46fcb8f86b42ae8515d45f297ab0a9
LucCha42 <55276692+LucCha42@users.noreply.github.com>
2019-11-08 15:48:40 +0100

    Mise à jour accueil

--- includes/pages/accueil.php
+++ includes/pages/accueil.php
@@ -2,11 +2,9 @@
 	<div class="row ligne0">
 
 		<div class="inner col-lg-5">
-			<h1>Partagez vos fichiers en toute tranquilité</h1>
-			<p>Chez Ki-Oui, l'hébergement de vos fichiers est crypté.<br/>
-			Partagez-les avec un lien auto-généré comme bon vous semble.<br/>
-			Créez-vous un compte pour répertorier vos fichiers.</p>
-			<a href="" class="button"><i class="fas fa-pen"></i> &nbsp; S'inscrire</a>
+			<h1>Partagez vos fichiers en toute <strong>sécurité</strong></h1>
+			<i class="fas fa-file-alt"></i>
+			<p><a href="" class="button"><i class="fas fa-pen"></i> &nbsp; S'inscrire</a></p>
 		</div>
 
 		<div class="inner col-lg-1"></div>
@@ -22,16 +20,19 @@
 		<div class="inner col-lg-4">
 			<h2>Partage Sécurisé</h2>
 			<i class="fas fa-unlock-alt"></i>
+			<p>Vos données sont cryptées,<br/>Ki-Oui ne pourra pas accéder à vos fichiers.</p>
 		</div>
 
 		<div class="inner col-lg-4">
 			<h2>Authentification Simple</h2>
 			<i class="fas fa-user"></i>
+			<p>Une adresse mail,<br/>un mote de passe.<br/>Rien de plus.</p>
 		</div>
 
 		<div class="inner col-lg-4">
 			<h2>Faire un don</h2>
 			<i class="fas fa-euro-sign"></i>
+			<p>Parce que nous sommes étudiants <3</p>
 		</div>
 
 	</div>
@@ -39,12 +40,18 @@
 
 
 
-<section class="pair">
+<section class="pair container-fluid">
+	<div class="row ligne2">
 
-	<div class="inner">
-		<a href="/ajout" class="button"><i class="fas fa-file-import"></i> &nbsp; Ajouter un fichier</a>
+		<div class="inner col">
+			<p>Chez Ki-Oui, l'hébergement de vos fichiers est crypté.<br/>
+			Partagez-les avec un lien auto-généré comme bon vous semble.<br/>
+			Créez-vous un compte pour répertorier vos fichiers.</p>
+			<br/>
+			<a href="/ajout" class="button"><i class="fas fa-file-import"></i> &nbsp; Ajouter un fichier</a>
+		</div>
+
 	</div>
-
 </section>
 
 
