0866f8dd8956a451adb1f7bd39d9e5c183f63f73
Rominos111 <39590270+Rominos111@users.noreply.github.com>
2019-11-17 03:28:06 +0100

    Partage de fichier

--- includes/pages/ajout-nok.php
+++
@@ -1,9 +0,0 @@
-<div class="accueil container-fluid">
-    <div class="row justify-content-center">
-        <div class="col-5 align-self-center box" style="text-align:center;">
-            <p>
-                Erreur d'ajout ! (<a href="/">cliquez ici</a> pour revenir à l'accueil)
-            </p>
-        </div>
-    </div>
-</div>
