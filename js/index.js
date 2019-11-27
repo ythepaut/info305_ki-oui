458db0ddb16a4dbe3c32232fe0bb9d8f2178de82
LucCha42 <55276692+LucCha42@users.noreply.github.com>
2019-11-27 20:57:13 +0100

    Système de changement de thème

--- js/index.js
+++ js/index.js
@@ -20,3 +20,80 @@ function editModalDirectDownload(path, key, originalName) {
     //document.querySelector("#originalName-directdownload").setAttribute("value", originalName);
     document.querySelector("#originalName-directdownload").innerHTML = originalName;
 }
+
+
+//=============================================================================
+
+// Foncitons pour les cookies <3
+
+function createCookie(name,value,days) {
+	if (days) {
+		var date = new Date();
+		date.setTime(date.getTime()+(days*24*60*60*1000));
+		var expires = "; expires="+date.toGMTString();
+	}
+	else var expires = "";
+	document.cookie = name+"="+value+expires+"; path=/";
+}
+
+function readCookie(name) {
+	var nameEQ = name + "=";
+	var ca = document.cookie.split(';');
+	for(var i=0;i < ca.length;i++) {
+		var c = ca[i];
+		while (c.charAt(0)==' ') c = c.substring(1,c.length);
+		if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+	}
+	return null;
+}
+
+function eraseCookie(name) {
+	createCookie(name,"",-1);
+}
+
+//=============================================================================
+
+
+/**
+ * Fonction qui créer un cookie conservant le thème
+ */
+function editModalTheme(theme) {
+	switch (theme) {
+		case 'kioui':
+			createCookie('theme', 'kioui', false);
+			break;
+		case 'frez':
+			createCookie('theme', 'frez', false);
+			break;
+		case 'dark':
+			createCookie('theme', 'dark', false);
+			break;
+	}
+	changeTheme();
+}
+
+
+/**
+ * Fonction qui modifie la deuxième balise css dans le header
+ */
+function changeTheme() {
+
+	var link = ".";
+
+	themeCookie = readCookie('theme');
+
+	switch (themeCookie) {
+		case 'kioui':
+			link = "";
+		break;
+		case 'frez':
+			link = "../css/theme-frez.css";
+		break;
+		case 'dark':
+			link = "../css/theme-dark.css";
+		break;
+	}
+	
+	document.getElementById('themeBis').href = link;
+}
+
