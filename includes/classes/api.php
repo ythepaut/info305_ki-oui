8bf2eeca4e5a58794d4a618c62b6788a6facf15b
Yohann THEPAUT <comptes@ythepaut.com>
2019-11-29 21:51:44 +0100

    Debut API + Interface administration

---
+++ includes/classes/api.php
@@ -0,0 +1,113 @@
+<?php
+//Fichier qui gère l'ensemble des requetes API publique
+
+session_start();
+
+include_once(getcwd() . "/config-db.php");
+include_once(getcwd() . "/config-email.php");
+include_once(getcwd() . "/config-recaptcha.php");
+include_once(getcwd() . "/utils.php");
+include_once(getcwd() . "/totp.php");
+include_once(getcwd() . "/u2f.php");
+require_once(getcwd() . '/PHPMailer/PHPMailerAutoload.php');
+
+/**
+ * Liste des erreurs :
+ * 
+ * ERROR_UNSAFE_CONNECTION          -       Connexion non sécurisée
+ * ERROR_INVALID_ARGUMENT           -       Argument de la requete invalide
+ * ERROR_UNKNOWN                    -       Erreur inconnue
+ * ERROR_INVALID_CREDENTIALS        -       Identifiants incorrects
+ * ERROR_ACCOUNT_INACCESSIBLE       -       Compte suspendu / Non validé / En attente de suppression / Niveau d'acces invalide
+ */
+
+if (!isset($_SERVER['HTTPS'])) {
+    die(json_encode(array("status" => "error", "error" => "ERROR_UNSAFE_CONNECTION", "verbose" => "HTTPS is required to execute API requests."), JSON_PRETTY_PRINT));
+}
+
+$action = (isset($_POST['action'])) ? $_POST['action'] : "";
+$action = ($action == "" && isset($_GET['action']) && $_GET['action'] != "") ? $_GET['action'] : $action;
+
+$query = (isset($_POST['action'])) ? $_POST : $_GET;
+
+switch ($action) {
+
+    case "auth":
+        die(json_encode(auth($query, $connection), JSON_PRETTY_PRINT));
+        break;
+
+    default:
+        die(json_encode(array("status" => "error", "error" => "ERROR_INVALID_ARGUMENT", "verbose" => "Missing or invalid argument `action`."), JSON_PRETTY_PRINT));
+        break;
+
+}
+
+
+/**
+ * Fonction qui authentifie un utilisateur et retourne un jeton de connexion
+ * (Requete API - Extension)
+ * 
+ * @param string            $query              -   Requete API
+ * @param mysqlconnection   $connection         -   Connexion BDD effectuée dans le fichier config-db.php          
+ * 
+ * @return array
+ */
+function auth($query, $connection) {
+
+    $result = array("status" => "error", "error" => "ERROR_UNKNOWN", "verbose" => "An unknown error occured during the process. This is our fault.");
+
+    if (isset($query['email'], $query['passwd'], $query['duration']) && filter_var($query['email'], FILTER_VALIDATE_EMAIL) && $query['duration'] > 0 && $query['duration'] < 2592000) {
+
+        //Recuperation des données
+        $dbquery = $connection->prepare("SELECT * FROM kioui_accounts WHERE email = ?");
+        $dbquery->bind_param("s", $query['email']);
+        $dbquery->execute();
+        $dbresult = $dbquery->get_result();
+        $dbquery->close();
+        $userData = $dbresult->fetch_assoc();
+
+        if (isset($userData['id']) && $userData['id'] != null && password_verify(hash('sha512', hash('sha512', $query['passwd'] . $userData['salt'])), $userData['password'])) {
+
+            //Verification du compte
+            if ($userData['access_level'] != "" && $userData['status'] == "ALIVE") {
+
+                $token = randomString(64);
+                $expire = time() + $query['duration'];
+
+                $dbquery = $connection->prepare("UPDATE kioui_accounts SET auth_token = ? , auth_token_expire = ? WHERE id = ?");
+                $dbquery->bind_param("sii", $token, $expire, $userData['id']);
+                $dbquery->execute();
+                $dbquery->close();
+
+                $result = array("status" => "success", "message" => "Your token has been generated.", "token" => $token);
+
+            } else {
+                $result = array("status" => "error", "error" => "ERROR_ACCOUNT_INACCESSIBLE", "verbose" => "This account is not validated or it is suspended or is in cancellation procedure.");
+            }
+
+        } else {
+            $result = array("status" => "error", "error" => "ERROR_INVALID_CREDENTIALS", "verbose" => "Arguments `email` and `passwd` do not match with any account.");
+        }
+
+    } else {
+        if (!isset($query['email']) || !filter_var($query['email'], FILTER_VALIDATE_EMAIL)) {
+            $result = array("status" => "error", "error" => "ERROR_INVALID_ARGUMENT", "verbose" => "Missing or invalid argument `email`.");
+        } elseif (!isset($query['passwd'])) {
+            $result = array("status" => "error", "error" => "ERROR_INVALID_ARGUMENT", "verbose" => "Missing or invalid argument `passwd`.");
+        } elseif (!isset($query['duration']) || $query['duration'] > 0 || $query['duration'] < 2592000) {
+            $result = array("status" => "error", "error" => "ERROR_INVALID_ARGUMENT", "verbose" => "Missing or invalid argument `duration`.");
+        }
+    }
+
+    return $result;
+
+}
+
+
+
+
+
+
+
+
+?>
--- includes/pages/espace-utilisateur/administration.php
+++ includes/pages/espace-utilisateur/administration.php
@@ -5,8 +5,38 @@
         <section class="col-lg panel-background">
         <div class="row">
 
-            <div class="col panel-outline">
-                <span>Interface administration <sub>(wow)</sub></span>
+            <div class="col panel-outline" style="padding: 0px;">
+                <!--Tab nav-->
+                <ul class="nav nav-tabs" role="tablist">
+                    <li class="nav-item">
+                        <a class="nav-link active link" style="border: 0px solid transparent;" id="stac-tab" data-toggle="tab" href="#stac" role="tab" aria-controls="stac" aria-selected="true">Statistiques et actions globales</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link link" style="border: 0px solid transparent;" id="gesut-tab" data-toggle="tab" href="#gesut" role="tab" aria-controls="gesut" aria-selected="false">Gestion des utilisateurs</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link link" style="border: 0px solid transparent;" id="gesfi-tab" data-toggle="tab" href="#gesfi" role="tab" aria-controls="gesfi" aria-selected="false">Gestion des fichiers</a>
+                    </li>
+                </ul>
+
+
+                <div class="col">
+
+                    <div class="tab-content" id="myTabContent">
+                        <div class="tab-pane fade show active" id="stac" role="tabpanel" aria-labelledby="stac-tab">
+                            Minim eiusmod ut culpa consectetur laborum cillum tempor magna non dolore voluptate aute nostrud. Deserunt sunt dolore aliquip in voluptate reprehenderit adipisicing irure quis ut nisi proident id. Aliqua eu velit dolor eiusmod et enim. Esse cillum do laboris aliqua nostrud et do aute dolore fugiat minim sunt aliqua. Anim proident quis id minim elit elit nisi cillum sunt eu.
+                        </div>
+                        <div class="tab-pane fade" id="gesut" role="tabpanel" aria-labelledby="gesut-tab">
+                            Aute pariatur consectetur aliquip nisi incididunt ex dolor sunt nisi eu adipisicing Lorem et sit. Laborum eu incididunt amet officia voluptate proident ullamco cillum minim est fugiat. Deserunt aute commodo culpa exercitation voluptate ex et sunt et ea laborum.
+                        </div>
+                        <div class="tab-pane fade" id="gesfi" role="tabpanel" aria-labelledby="gesfi-tab">
+                            In ad eiusmod consectetur in culpa labore incididunt qui laboris irure duis cillum quis. Mollit in cupidatat deserunt aliqua. Ut Lorem velit ea ea consectetur et sunt laboris nostrud et laborum commodo ullamco. Reprehenderit veniam mollit nisi dolore proident Lorem anim excepteur voluptate deserunt eu fugiat. Et culpa dolor dolor ipsum dolor eu occaecat voluptate qui aute aute amet sunt. Qui laborum sit non tempor reprehenderit do incididunt aliquip consectetur excepteur.
+                        </div>
+                    </div>
+                    
+
+
+                </div>
             </div>
 
         </div>
