a6b651b5fe1ebbf7c2d78b931e4e126cce49721a
LucCha42 <55276692+LucCha42@users.noreply.github.com>
2019-12-05 09:19:07 +0100

    Seciton fichier espace admin
    Est finis :smile:  (je crois)

--- includes/pages/espace-utilisateur/administration/fichiers.php
+++ includes/pages/espace-utilisateur/administration/fichiers.php
@@ -5,37 +5,53 @@
         $res = "<table class='table'>";
         $res .= "<thead class='thead'>";
 
-        $res .= "<th style='width:auto'>Nom du fichier</th>";
-        $res .= "<th style='width:25%'>Taille</th>";
-        $res .= "<th style='width:25%'>Propriétaire</th>";
-        $res .= "<th style='width:25%'>Date d'ajout</th>";
+        $res .= "<th style='width:10%'>Id du fichier</th>";
+        $res .= "<th style='width:20%'>Chemin</th>";
+        $res .= "<th style='width:20%'>Propriétaire</th>";
+        $res .= "<th style='width:20%'>Adresse IP</th>";
+        $res .= "<th style='width:20%'>Taille</th>";
+        $res .= "<th style='width:20%'>Date d'ajout</th>";
         $res .= "<th style='width:auto'>Actions</th>";
 
         $res .= "</thead>";
 
         while ($file = mysqli_fetch_assoc($query)) {
 
-        	//$owner = mysql_fetch_object(mysqli_query($connection, "SELECT username FROM kioui_accounts WHERE id = $file["owner"] limit 1"));
+        	//Récupération du propriétaire
+            $query2 = $connection->prepare("SELECT username FROM kioui_accounts WHERE id = ?");
+            $query2->bind_param("s", $file["owner"]);
+            $query2->execute();
+            $result = $query2->get_result();
+            $query2->close();
+            $owner = $result->fetch_assoc();
 
             $res .= "<tr>";
-            //Affichage du nom du ficher
+            //Affichage de l'id du ficher
             $res .= "<td>";
-            $res .= $file["original_name"]; // A CHANGER !!!
+            $res .= $file["id"];
+            $res .= "</td>";
+            //Affichage du chemmin du fichier
+            $res .= "<td>";
+            $res .= $file["path"];
+            $res .= "</td>";
+            //Affichage du propriétaire
+            $res .= "<td>";
+            $res .= implode( $owner );
+            $res .= "</td>";
+            //Affichage de l'ip du prorpio
+            $res .= "<td>";
+            $res .= $file["ip"];
             $res .= "</td>";
             //Affichage de la taille du fichier
             $res .= "<td>";
             $res .= convertUnits($file["size"]);
-            $res .= "</td>";
-            //Affichage du propriétaire
-            $res .= "<td>";
-            $res .= $file["owner"]; // A CHANGER !!!
             $res .= "</td>";
             //Affichage de la date d'upload
             $res .= "<td>";
             $res .= date("d/m/Y", $file["upload_date"]);
             $res .= "</td>";
             //Action
-            $res .= "<td>";
+            $res .= "<td style='text-align:center'>";
             $res .= "<a href='#' title=\"Supprimer le fichier\" data-toggle='modal' data-target='#modalDeleteFile' onclick='editModalDelete(" . $file['id'] . ")'><i class='fas fa-trash-alt delete'></i></a>&nbsp;";
             $res .= "</td>";
 
